ac2dff73bf7fd6d4b85959717cc2d39ed3d180e7
Muhammad Rafi Walidain <rafi.walidain81@gmail.com>
2025-03-31 08:29:13 +0700

    mvc

---
+++ app/controllers/About.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+
+class About{
+    public function index($nama = "rafi", $jobs = "programmer"){
+        echo "halo saya $nama dan saya $jobs";
+    }
+
+    public function page(){
+        echo "About/page";
+    }
+}
---
+++ publik/Index.php
@@ -0,0 +1,10 @@
+<?php 
+
+require_once '../app/Init.php';
+
+$app = new App();
+
+
+
+
+?>
