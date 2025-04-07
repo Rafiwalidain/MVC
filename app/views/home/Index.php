30e78031b017dad1424f3c1d4f4faa92d2f39b3b
Muhammad Rafi Walidain <rafi.walidain81@gmail.com>
2025-04-07 14:17:13 +0700

    wrep

--- app/views/home/Index.php
+++ app/views/home/Index.php
@@ -1,3 +1,4 @@
 <div class="container">
-    <h1 class="mt-3">Selamat datang di sini</h1>
+    <h1 class="mt-3">Selamat datang di website ini.</h1>
+    <h3>Halo nama saya <?php echo $data["nama"]; ?></h3>
 </div>
