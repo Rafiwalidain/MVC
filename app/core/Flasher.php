ea8a1b7823c0f880f8a2227c22488a2267685e5a
Muhammad Rafi Walidain <rafi.walidain81@gmail.com>
2025-04-09 18:34:03 +0700

    flash

--- app/core/Flasher.php
+++ app/core/Flasher.php
@@ -15,9 +15,9 @@ class Flasher
     {
         if (isset($_SESSION['flash'])) {
             echo '<div class="alert alert-' . $_SESSION['flash']['tipe'] . ' alert-dismissible fade show" role="alert">Data Mahasiswa
-  <strong>' . $_SESSION['flash']['pesan'] . '</strong> ' . $_SESSION['flash']['aksi'] . '
-  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>';
+            <strong>' . $_SESSION['flash']['pesan'] . '</strong> ' . $_SESSION['flash']['aksi'] . '
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>';
 
             unset($_SESSION['flash']);
         }
--- publik/Index.php
+++ publik/Index.php
@@ -1,10 +1,7 @@
-<?php 
+<?php
+
+if (!session_id()) session_start();
 
 require_once '../app/Init.php';
 
 $app = new App();
-
-
-
-
-?>
